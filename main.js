e4ec1118dde73ffc0c5f29539cbe83fb4fbca438
Gleb Fedotov <innercoding@gmail.com>
2024-08-21 12:32:34 +0300

    simple bot

---
+++ main.js
@@ -0,0 +1,21 @@
+import { Telegraf, Markup } from "telegraf";
+import "dotenv/config";
+
+const token = process.env.bot_token;
+const web_app_url = process.env.web_app_url;
+const message = process.env.message;
+const button_title = process.env.button_title;
+
+const bot = new Telegraf(token);
+
+const inlineMessageRatingKeyboard = Markup.inlineKeyboard([
+  Markup.button.webApp(button_title, web_app_url),
+]);
+
+const sendBtn = (ctx) =>
+  ctx.telegram.sendMessage(ctx.chat.id, message, inlineMessageRatingKeyboard);
+
+bot.command("start", sendBtn);
+bot.on("message", sendBtn);
+
+bot.launch();
